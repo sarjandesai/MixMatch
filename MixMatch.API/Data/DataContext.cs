2a5784a90b98bb714972b773ba7c5de87ee2e8ab
Desai <sd185272@ncr.com>
2018-11-09 08:50:15 +1100

    initial commit

---
+++ MixMatch.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MixMatch.API.Models;
+
+namespace MixMatch.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+       
+       public DbSet<Value> Values {get; set;}
+    }
+}
---
+++ MixMatch.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace MixMatch.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
